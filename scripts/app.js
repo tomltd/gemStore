a52bbad7e7599b35b450950c3d4d5f1ec3f1602c
tomltd <tomfreestone@gmail.com>
2016-02-16 14:09:15 +0000

    add image

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,53 @@
   });
 
   var gems = [
-    { name: 'Azurite', price: 2.95, canPurchase: true, soldOut: false },
-    { name: 'Bloodstone', price: 5.95, canPurchase: true, soldOut: false },
-    { name: 'Zircon', price: 3.95, canPurchase: true, soldOut: false }
+    { name: 'Azurite',
+      price: 2.95,
+      description: 'This gem has lots of sides',
+      images: [
+        {
+          full: 'http://placebear.com/300/300',
+          thumb: 'http://placebear.com/100/100'
+        },
+        {
+          full: 'http://placebear.com/300/200',
+          thumb: 'http://placebear.com/50/50'
+        }
+      ],
+      canPurchase: true,
+      soldOut: false
+    },
+    { name: 'Bloodstone',
+      price: 5.95,
+      description: 'This gem has lots of sides',
+      images: [
+        {
+          full: 'http://placebear.com/300/300',
+          thumb: 'http://placebear.com/100/100'
+        },
+        {
+          full: 'http://placebear.com/300/200',
+          thumb: 'http://placebear.com/50/50'
+        }
+      ],
+      canPurchase: true,
+      soldOut: false
+    },
+    { name: 'Zircon',
+      price: 3.95,
+      description: 'This gem has lots of sides',
+      images: [
+        {
+          full: 'http://placebear.com/300/300',
+          thumb: 'http://placebear.com/100/100'
+        },
+        {
+          full: 'http://placebear.com/300/200',
+          thumb: 'http://placebear.com/50/50'
+        }
+      ],
+      canPurchase: true,
+      soldOut: false
+    }
   ];
 })();
